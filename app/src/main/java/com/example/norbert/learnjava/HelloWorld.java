53087342e6a344b6389b5d0c86867eebb0d8466b
norbertblaise <lukwayi@gmail.com>
2022-04-23 13:22:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/norbert/learnjava/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.norbert.learnjava;
+
+/**
+ * Created by Norbert on 12/9/2017.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My first Java Program");
+
+        String tim = "Tim Buchalka";
+        System.out.println(tim);
+    }
+}
